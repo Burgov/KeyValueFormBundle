07f31335a4eda63beeab020548828a2e00e7d6a0
WouterJ <waldio.webdesign@gmail.com>
2016-05-31 13:58:16 +0200

    Also fix KeyValueRowType

--- Tests/Form/Type/KeyValueTypeTest.php
+++ Tests/Form/Type/KeyValueTypeTest.php
@@ -6,10 +6,36 @@ use Burgov\Bundle\KeyValueFormBundle\Form\Type\KeyValueRowType;
 use Burgov\Bundle\KeyValueFormBundle\Form\Type\KeyValueType;
 use Symfony\Component\Form\AbstractExtension;
 use Symfony\Component\Form\Extension\Core\ChoiceList\ObjectChoiceList;
+use Symfony\Component\Form\ChoiceList\ArrayChoiceList;
 use Symfony\Component\Form\Test\TypeTestCase;
 
 class KeyValueTypeTest extends TypeTestCase
 {
+    private static $isSf28;
+    private static $typeNames = array();
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        if (null === self::$isSf28) {
+            self::$isSf28 = method_exists('Symfony\Component\Form\AbstractType', 'getBlockPrefix');
+            if (self::$isSf28) {
+                self::$typeNames = array(
+                    'choice' => 'Symfony\Component\Form\Extension\Core\Type\ChoiceType',
+                    'text' => 'Symfony\Component\Form\Extension\Core\Type\TextType',
+                    'burgov_key_value' => 'Burgov\Bundle\KeyValueFormBundle\Form\Type\KeyValueType',
+                );
+            } else {
+                self::$typeNames = array(
+                    'choice' => 'choice',
+                    'text' => 'text',
+                    'burgov_key_value' => 'burgov_key_value',
+                );
+            }
+        }
+    }
+
     public function getExtensions()
     {
         return array(new ConcreteExtension());
@@ -43,20 +69,23 @@ class KeyValueTypeTest extends TypeTestCase
             'key3' => '1',
         );
 
-        $builder = $this->factory->createBuilder('burgov_key_value', $originalData, array(
-            'value_type' => 'text',
-            'key_options' => array('label' => 'label_key'),
-            'value_options' => array('label' => 'label_value')));
+        $form = $this->factory->create(
+            self::$typeNames['burgov_key_value'],
+            $originalData,
+            array(
+                'value_type' => self::$typeNames['text'],
+                'key_options' => array('label' => 'label_key'),
+                'value_options' => array('label' => 'label_value')
+            )
+        );
 
-        $form = $builder->getForm();
-
-        $this->assertFormTypes(array('text', 'text'), array('text', 'text'), $form);
+        $this->assertFormTypes(array(self::$typeNames['text'], self::$typeNames['text']), array(self::$typeNames['text'], self::$typeNames['text']), $form);
         $this->assertFormOptions(array(array('label' => 'label_key'), array('label' => 'label_value')), $form);
 
         $form->submit($submitData);
-        $this->assertTrue($form->isValid(), $form->getErrorsAsString());
+        $this->assertTrue($form->isValid(), $form->getErrors());
 
-        $this->assertFormTypes(array('text', 'text', 'text'), array('text', 'text', 'text'), $form);
+        $this->assertFormTypes(array(self::$typeNames['text'], self::$typeNames['text'], self::$typeNames['text']), array(self::$typeNames['text'], self::$typeNames['text'], self::$typeNames['text']), $form);
         $this->assertFormOptions(array(array('label' => 'label_key'), array('label' => 'label_value')), $form);
 
         $this->assertSame($expectedData, $form->getData());
@@ -72,15 +101,26 @@ class KeyValueTypeTest extends TypeTestCase
         $obj2->id = 2;
         $obj2->name = 'choice2';
 
-        $builder = $this->factory->createBuilder('burgov_key_value', null, array(
-            'value_type' => 'choice',
-            'key_options' => array('label' => 'label_key'),
-            'value_options' => array(
-                'choice_list' => new ObjectChoiceList(array($obj1, $obj2), 'name', array(), null, 'id'),
-                'label' => 'label_value'
-            )));
+        $valueOptions = array('label' => 'label_value');
+        if (self::$isSf28) {
+            // Symfony 2.8+
+            $valueOptions['choices'] = array($obj1, $obj2);
+            $valueOptions['choice_name'] = 'id';
+            $valueOptions['choice_value'] = 'name';
+        } else {
+            // Symfony <2.8
+            $valueOptions['choice_list'] = new ObjectChoiceList(array($obj1, $obj2), 'name', array(), null, 'id');
+        }
 
-        $form = $builder->getForm();
+        $form = $this->factory->create(
+            self::$typeNames['burgov_key_value'],
+            null,
+            array(
+                'value_type' => self::$typeNames['choice'],
+                'key_options' => array('label' => 'label_key'),
+                'value_options' => $valueOptions,
+            )
+        );
 
         $this->assertFormTypes(array(), array(), $form);
         $this->assertFormOptions(array(array('label' => 'label_key'), array('label' => 'label_value')), $form);
@@ -96,7 +136,7 @@ class KeyValueTypeTest extends TypeTestCase
             )
         ));
 
-        $this->assertFormTypes(array('text', 'text'), array('choice', 'choice'), $form);
+        $this->assertFormTypes(array(self::$typeNames['text'], self::$typeNames['text']), array(self::$typeNames['choice'], self::$typeNames['choice']), $form);
         $this->assertFormOptions(array(array('label' => 'label_key'), array('label' => 'label_value')), $form);
 
         $this->assertTrue($form->isValid());
@@ -106,13 +146,11 @@ class KeyValueTypeTest extends TypeTestCase
 
     public function testWithCustomKeyType()
     {
-        $builder = $this->factory->createBuilder('burgov_key_value', null, array(
+        $form = $this->factory->create(self::$typeNames['burgov_key_value'], null, array(
             'key_type' => 'country',
             'value_type' => 'integer',
             'key_options' => array('label' => 'label_key'),
         ));
-
-        $form = $builder->getForm();
 
         $this->assertFormTypes(array(), array(), $form);
         $this->assertFormOptions(array(array('label' => 'label_key'), array()), $form);
@@ -141,10 +179,11 @@ class KeyValueTypeTest extends TypeTestCase
         $this->assertCount(count($values), $form);
         for ($i = 0; $i < count($form); $i++) {
             if (isset($keys[$i])) {
-                $this->assertEquals($keys[$i], $form->get($i)->get('key')->getConfig()->getType()->getInnerType()->getName());
+                
+                $this->assertInstanceOf($keys[$i], $form->get($i)->get('key')->getConfig()->getType()->getInnerType());
             }
             if (isset($values[$i])) {
-                $this->assertEquals($values[$i], $form->get($i)->get('value')->getConfig()->getType()->getInnerType()->getName());
+                $this->assertInstanceOf($values[$i], $form->get($i)->get('value')->getConfig()->getType()->getInnerType());
             }
         }
     }
